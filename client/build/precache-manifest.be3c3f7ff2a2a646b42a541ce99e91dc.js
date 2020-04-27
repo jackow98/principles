eedb35b523eceab1b8e773da76717e9eeb7a64d7
jackow98 <jw50@hw.ac.uk>
2020-04-27 17:43:15 +0100

    Added build

---
+++ client/build/precache-manifest.be3c3f7ff2a2a646b42a541ce99e91dc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa535c47340b6fd1e30b1a5e8537b511",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b722dfdc248a031ea58",
+    "url": "/static/css/main.662e00b9.chunk.css"
+  },
+  {
+    "revision": "309f156891edf804bfbe",
+    "url": "/static/js/2.25eaae97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.25eaae97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b722dfdc248a031ea58",
+    "url": "/static/js/main.183db1e3.chunk.js"
+  },
+  {
+    "revision": "a85f59fe95472cf9d623",
+    "url": "/static/js/runtime-main.ff720301.js"
+  },
+  {
+    "revision": "ddc250127189a3b4ee4802e6c2493757",
+    "url": "/static/media/github.ddc25012.svg"
+  },
+  {
+    "revision": "9ee7794d35df0b65d706bee9fab1abf0",
+    "url": "/static/media/notion-logo.9ee7794d.svg"
+  },
+  {
+    "revision": "23cdb344292769bc992f1078a18488a6",
+    "url": "/static/media/search.23cdb344.svg"
+  }
+]);
